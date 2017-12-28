9a3eb0e195c2f03bd44db51108eee0a86f58d396
Luay <luayghazwan@yahoo.com>
2017-12-28 14:32:49 -0800

    New methods in Code Fellows class

--- CodeFellowsApp/CodeFellowsApp/Classes/CodeFellows.cs
+++ CodeFellowsApp/CodeFellowsApp/Classes/CodeFellows.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CodeFellowsApp.Classes
 {
     // The class is abstract which means it can not be instantiated but the derived classes will use its methods and properties
     // Public access modifier means this class can be accessed from anywhere in the computer/website/service
-    public  abstract class CodeFellows
+    public abstract class CodeFellows
     {
         public string SchoolName { get; set; }
         public string Location { get; set; }
         public string Industry { get; set; }
+        public string CodeOfConduct { get; set; }
 
-        public string CodeOfConduct()
+        public string GetCodeOfConduct()
         {
-            string phrase =
-                "We lead with humility and empathy to create safe and inclusive environments where people learn and lives are transformed.";
-            return phrase;
+            return this.CodeOfConduct;
+        }
+
+        public string GetScheduleToday()
+        {
+            string today = $"Come to Code Fellows at this location: {Location}";
+            return today;
+        }
+
+        public bool IsOpenToday(string day)
+        {
+            string[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday","Firday"};
+            return weekdays.Contains(day);
         }
     }
 }
