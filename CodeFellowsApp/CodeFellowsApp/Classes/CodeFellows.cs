89560f8c93ad0ca54ca4ce481e6ae5e1a3e3c177
Luay <luayghazwan@yahoo.com>
2017-12-27 23:50:48 -0800

    Code Fellows Abstract class

---
+++ CodeFellowsApp/CodeFellowsApp/Classes/CodeFellows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFellowsApp.Classes
+{
+    // The class is abstract which means it can not be instantiated but the derived classes will use its methods and properties
+    // Public access modifier means this class can be accessed from anywhere in the computer/website/service
+    public  abstract class CodeFellows
+    {
+        public string SchoolName { get; set; }
+        public string Location { get; set; }
+        public string Industry { get; set; }
+
+        public string CodeOfConduct()
+        {
+            string phrase =
+                "We lead with humility and empathy to create safe and inclusive environments where people learn and lives are transformed.";
+            return phrase;
+        }
+    }
+}
