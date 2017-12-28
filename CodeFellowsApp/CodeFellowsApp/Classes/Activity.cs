59005f09a410686e3520e1522c19579231d98e3f
Luay <luayghazwan@yahoo.com>
2017-12-28 00:29:11 -0800

    Activity class derived from Code Fellows abstract class

---
+++ CodeFellowsApp/CodeFellowsApp/Classes/Activity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeFellowsApp.Classes
+{
+    // Derived class from Code Fellows Abstract base class
+    internal class Activity : CodeFellows
+    {
+        public int PowerHourAttendees { get; set; }
+
+        // Private properties for Ping Pong players
+        private Student playerOne { get; set; }
+        private Student playerTwo { get; set; }
+
+        // public method for activity to use when instantiated
+        public void PlayPingPing(Student studentOne, Student studentTwo)
+        {
+            this.playerOne = studentOne;
+            this.playerTwo = studentTwo;
+            Console.WriteLine($"{playerOne} plays against {playerTwo} every day!");
+        }
+
+        // Power hour segment every friday
+        public void WatchLearnFromPowerHour()
+        {
+            Console.WriteLine("There are {0} attendees at this power hour.", this.PowerHourAttendees);
+        }
+    }
+}
