cd51d30a7073814194c6dd07ede8da24d4081b34
Luay <luayghazwan@yahoo.com>
2017-12-28 00:08:11 -0800

    Class room class derived class from CodeFellows base class

---
+++ CodeFellowsApp/CodeFellowsApp/Classes/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFellowsApp.Classes
+{
+    internal class ClassRoom : CodeFellows
+    {
+        // Proerties that complete a class room at the Code fellows
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public bool Available { get; set; }
+        public int NumberOfChairs { get; set; }
+        public int NumberOfTables { get; set; }
+        public bool ProjectorAvailable { get; set; }
+        public bool WhiteboardAvailable { get; set; }
+
+        // Cohort is a class type
+        public Cohort CurrentCohort { get; set; }
+    }
+}
