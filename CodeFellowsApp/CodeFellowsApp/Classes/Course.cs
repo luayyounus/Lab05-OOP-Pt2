f7e0d670e2b4c16316eaf075b836b69fad31485d
Luay <luayghazwan@yahoo.com>
2017-12-27 23:51:46 -0800

    Course abstract class that represents a blueprint to be derived by other classes

---
+++ CodeFellowsApp/CodeFellowsApp/Classes/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFellowsApp.Classes
+{
+    // The class is abstract which means it can not be instantiated but the derived classes will use its methods and properties
+    internal abstract class Course : CodeFellows
+    {
+        // Properties to be assigned in the derived classes
+        public byte Code { get; set; }
+        public int CoursePrice { get; set; }
+        public int CourseLengthInWeeks { get; set; }
+        public string[] TechnologiesUsed { get; set; }
+        public Student[] StudentsEnrolled { get; set; }
+        public Instructor Instructor { get; set; }
+        public TeacherAssistant[] TeacherAssistant { get; set; }
+
+        // Defining Abstract Method to be implemented in the derived classes
+        public abstract int CheckDifficulty(string[] technologiesUsed);
+
+        // Virtual method implemented to be optionally used in the derived classes
+        public virtual int Discount(int percentage)
+        {
+            // Return the price from this class property
+            return this.CoursePrice * percentage / 100;
+        }
+    }
+}
