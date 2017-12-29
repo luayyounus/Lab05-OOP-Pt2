6af4653787cb23d82e9be514914b9a42398514a5
Luay <luayghazwan@yahoo.com>
2017-12-28 16:32:54 -0800

    Test implemented to test Virtual Discount method in Dot Net

---
+++ CodeFellowsApp/XUnitTestProjectCodeFellows/UnitTest1.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+using CodeFellowsApp.Classes;
+
+namespace XUnitTestProjectCodeFellows
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(5000, 10000, 50, 401, "Luay")]
+        [InlineData(2400, 6000, 40, 901, "Amanda")]
+        public void ReturnDiscountFromDotNet(int exptectedPrice, int coursePrice, int discount, int code, string studentName)
+        {
+            // Arrange
+            Instructor instructor = new Instructor
+            {
+                Name = "Amanda",
+                FavoriteProgrammingLanguage = "Csharp",
+                Location = "Down town, Seattle"
+            };
+
+            DotNet dotNet = new DotNet(code, studentName, instructor);
+            dotNet.CoursePrice = coursePrice;
+
+            // Act
+            int actualPrice = dotNet.Discount(discount);
+
+            // Assert
+            Assert.Equal(exptectedPrice, actualPrice);
+        }
+    }
+}
